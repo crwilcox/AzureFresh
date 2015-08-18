f8f05aece0ca73c1a9b50cb606203a69f25d602f
Christopher Wilcox <git@crwilcox.com>
2015-08-18 12:58:00 -0700

    Add test_product_model template
    This file has one test that isn't implemented and can be added by the
    user.  The idea is to add the following code:
    p = Product(id=1)
    r = p.recommended_items
    self.assertIsNotNone(r)
    self.assertEqual(r[0].id, 2)

---
+++ tests/test_product_model.py
@@ -0,0 +1,9 @@
+﻿import unittest
+#from app.models import Product
+
+class TestProductModel(unittest.TestCase):
+    def test_get_recommendations(self):
+        self.fail("Not Implemented")
+
+if __name__ == '__main__':
+    unittest.main()
